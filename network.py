d0b8e2289978eb75535e81d317646323860a07b4
Aidan <datbluemushroom@gmail.com>
2023-02-07 22:06:49 -0500

    ✨ Training and Predictions

--- network.py
+++ network.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib as plt
 
 
 # retrieve testing and training data
@@ -118,6 +119,18 @@ def make_predictions(X, W1, b1, W2, b2):
     predictions = get_predictions(A2)
 
     return predictions
+
+def test_prediction(index, W1, b1, W2, b2):
+    current_image = X_train[:, index, None]
+    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
+    label = Y_train[index]
+    print("Prediction: ", prediction)
+    print("Label: ", label)
+    
+    current_image = current_image.reshape((28, 28)) * 255
+    plt.gray()
+    plt.imshow(current_image, interpolation='nearest')
+    plt.show()
 
 # update parameters
 def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
